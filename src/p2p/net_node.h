cc56caa53da1e11a9b99f252b760021d2b116797
zooteld <zooteld@gmail.com>
2019-06-22 13:51:41 -0500

    Update net_node.h

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -155,14 +155,12 @@ class node_server : public epee::levin::levin_commands_handler<p2p_connection_co
 
   private:
 	const std::vector<std::string> m_seed_nodes_list =
-		{"seed1.ryo-currency.com",
-		 "seed2.ryo-currency.com",
-		 "seed3.ryo-currency.com",
-		 "seed1.ryo-currency.org",
-		 "seed2.ryo-currency.org",
-		 "seed3.ryo-currency.org",
-		 "testnet-seed.ryo-currency.com",
-		 "testnet-seed.ryo-currency.org"};
+		{ "178.128.181.80",
+	  "185.181.61.14",
+	  "5.56.132.162",
+	  "185.113.115.13",
+      "173.212.213.63"
+    };
 
 	bool islimitup = false;
 	bool islimitdown = false;
