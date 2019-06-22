bfaef767171468f760e3496db686aa94cfafe48e
zooteld <zooteld@gmail.com>
2019-06-22 13:15:29 -0500

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -52,7 +52,7 @@
 
 #define CRYPTONOTE_DNS_TIMEOUT_MS 20000
 
-#define CRYPTONOTE_GETBLOCKTEMPLATE_MAX_BLOCK_SIZE 196608 //size of block (bytes) that is the maximum that miners will produce
+#define CRYPTONOTE_GETBLOCKTEMPLATE_MAX_BLOCK_SIZE 500000000 //size of block (bytes) that is the maximum that miners will produce
 #define CRYPTONOTE_PUBLIC_ADDRESS_TEXTBLOB_VER 0
 #define CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW 60
 #define CURRENT_TRANSACTION_VERSION 3
@@ -61,10 +61,10 @@
 #define CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE 10
 
 // MONEY_SUPPLY - total number coins to be generated
-#define MONEY_SUPPLY ((uint64_t)88888888000000000)
-#define EMISSION_SPEED_FACTOR 19
-#define FINAL_SUBSIDY ((uint64_t)4000000000)			  // 4 * pow(10, 9)
-#define GENESIS_BLOCK_REWARD ((uint64_t)8800000000000000) // ~10% dev premine, now  mostly burned
+#define MONEY_SUPPLY ((uint64_t)500000000000000000)		// 0.5 billion.
+#define EMISSION_SPEED_FACTOR 21
+#define FINAL_SUBSIDY ((uint64_t)10000000000)			  // 1 * pow(10, 9)
+#define GENESIS_BLOCK_REWARD ((uint64_t)1000000000) // 
 #define EMISSION_SPEED_FACTOR_PER_MINUTE (20)
 
 #define CRYPTONOTE_REWARD_BLOCKS_WINDOW 60
@@ -83,7 +83,7 @@
 #define CRYPTONOTE_MEMPOOL_TX_LIVETIME 86400				 //seconds, one day
 #define CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME 604800 //seconds, one week
 
-#define COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT 250
+#define COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT 1000
 
 #define P2P_LOCAL_WHITE_PEERLIST_LIMIT 1000
 #define P2P_LOCAL_GRAY_PEERLIST_LIMIT 5000
@@ -109,10 +109,10 @@
 
 #define ALLOW_DEBUG_COMMANDS
 
-#define CRYPTONOTE_NAME "ryo"
+#define CRYPTONOTE_NAME "ombre"
 #define CRYPTONOTE_POOLDATA_FILENAME "poolstate.bin"
-#define CRYPTONOTE_BLOCKCHAINDATA_FILENAME "data.mdb"
-#define CRYPTONOTE_BLOCKCHAINDATA_LOCK_FILENAME "lock.mdb"
+#define CRYPTONOTE_BLOCKCHAINDATA_FILENAME "blockchain.bin"
+#define CRYPTONOTE_BLOCKCHAINDATA_LOCK_FILENAME "blockchain.bin.lock"
 #define P2P_NET_DATA_FILENAME "p2pstate.bin"
 #define MINER_CONFIG_FILE_NAME "miner_conf.json"
 
@@ -130,7 +130,7 @@
 #define PEAK_COIN_EMISSION_YEAR 4
 #define PEAK_COIN_EMISSION_HEIGHT ((uint64_t)(((12 * 30.4375 * 24 * 3600) / common_config::DIFFICULTY_TARGET) * PEAK_COIN_EMISSION_YEAR)) // = (# of heights emmitted per year) * PEAK_COIN_EMISSION_YEAR
 
-#define TX_FORK_ID_STR "ryo-currency"
+#define TX_FORK_ID_STR "Ombre"
 
 template <typename T, std::size_t N>
 constexpr std::size_t countof(T const (&)[N]) noexcept
@@ -204,15 +204,15 @@ inline constexpr uint64_t MK_COINS(uint64_t coins) { return coins * 1000000000ul
 
 struct common_config
 {
-	static constexpr size_t MIN_MIXIN_V1 = 12; // default & minimum mixin allowed
-	static constexpr size_t MIN_MIXIN_V2 = 24;
+	static constexpr size_t MIN_MIXIN_V1 = 10; // default & minimum mixin allowed
+	static constexpr size_t MIN_MIXIN_V2 = 10;
 	static constexpr size_t MAX_MIXIN = 240;
 
 	static constexpr uint64_t POISSON_CHECK_TRIGGER = 10;  // Reorg size that triggers poisson timestamp check
 	static constexpr uint64_t POISSON_CHECK_DEPTH = 60;   // Main-chain depth of the poisson check. The attacker will have to tamper 50% of those blocks
 	static constexpr double POISSON_LOG_P_REJECT = -75.0; // Reject reorg if the probablity that the timestamps are genuine is below e^x, -75 = 10^-33
 
-	static constexpr uint64_t DIFFICULTY_TARGET = 240; // 4 minutes
+	static constexpr uint64_t DIFFICULTY_TARGET = 60; // 1 minutes
 
 	/////////////// V1 difficulty constants
 	static constexpr uint64_t DIFFICULTY_WINDOW_V1 = 720; // blocks
@@ -253,13 +253,13 @@ struct common_config
 
 	///////////////// Dev fund constants
 	// 2 out of 3 multisig address held by fireice, mosu, and psychocrypt
-	static constexpr const char* DEV_FUND_ADDRESS = "RYoLshTYzNEaizPi9jWtRtNPtan5fAqc3TVbyzZDWghLY99eXivKD1XQMseVyJQ1kD5FXDvk4nHqpUXBTMCm5aqmQU8tHaN51Wc";
+	static constexpr const char* DEV_FUND_ADDRESS = "cashCdYTudG44DDXfmWFxv9mFBbZ4rmaB2HcB5uCWQRXfdnjSBf8CDT3d8KtR4vAos8U3YrJKqu3CGacg6iQSqDR1PTekywzyt";
 	// 34d6b7155d99da44c3a73424c60ecb0da53d228ed8da026df00ed275ea54e803
-	static constexpr const char* DEV_FUND_VIEWKEY = "\x34\xd6\xb7\x15\x5d\x99\xda\x44\xc3\xa7\x34\x24\xc6\x0e\xcb\x0d\xa5\x3d\x22\x8e\xd8\xda\x02\x6d\xf0\x0e\xd2\x75\xea\x54\xe8\x03";
+	static constexpr const char* DEV_FUND_VIEWKEY = "bffa803bb4061d93a83a3613ff478c5b5e5cb0b0337a735e1a1304ca9fab1807";
 	// Exact number of coins burned in the premine burn, in atomic units
-	static constexpr uint64_t PREMINE_BURN_AMOUNT = 8700051446427001;
-	// Ryo donation address
-	static constexpr const char* RYO_DONATION_ADDR = "RYoLshssqU9WvHMwAmt4j6dtpgRERDqwzSiHF4V9nEb5YWmQ5pLSkJC9QudNseKrxBacKtQuLWhpSQ6GLXgyDWjKAGjNXH72VDJ";
+	static constexpr uint64_t PREMINE_BURN_AMOUNT = 1000000000;
+	// Omb donation address
+	static constexpr const char* RYO_DONATION_ADDR = "cashCdYTudG44DDXfmWFxv9mFBbZ4rmaB2HcB5uCWQRXfdnjSBf8CDT3d8KtR4vAos8U3YrJKqu3CGacg6iQSqDR1PTekywzyt";
 };
 
 template <network_type type>
@@ -270,25 +270,25 @@ struct config : public common_config
 template <>
 struct config<MAINNET>
 {
-	static constexpr uint64_t LEGACY_LONG_ADDRESS_BASE58_PREFIX = 0x2bb39a;			   // Sumo
-	static constexpr uint64_t LEGACY_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x29339a; //Sumi
-	static constexpr uint64_t LEGACY_LONG_SUBADDRESS_BASE58_PREFIX = 0x8319a;		   // Subo
+	static constexpr uint64_t LEGACY_LONG_ADDRESS_BASE58_PREFIX = 0xe1f54;			   // cash
+	static constexpr uint64_t LEGACY_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0xe9f54; //casi
+	static constexpr uint64_t LEGACY_LONG_SUBADDRESS_BASE58_PREFIX = 0x1b19a;		   // Suba
 
-	static constexpr uint64_t RYO_KURZ_SUBADDRESS_BASE58_PREFIX = 0x3fe192;			// RYo
-	static constexpr uint64_t RYO_KURZ_ADDRESS_BASE58_PREFIX = 0x2c6192;			// RYoK
-	static constexpr uint64_t RYO_LONG_ADDRESS_BASE58_PREFIX = 0x2ce192;			// RYoL
-	static constexpr uint64_t RYO_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x2de192; // RYoN
-	static constexpr uint64_t RYO_LONG_SUBADDRESS_BASE58_PREFIX = 0x2fe192;			// RYoS
+	static constexpr uint64_t RYO_KURZ_SUBADDRESS_BASE58_PREFIX = 0xcd18fd299;			// ShaDoW
+	static constexpr uint64_t RYO_KURZ_ADDRESS_BASE58_PREFIX = 0x11cfd299;			// ShaDoe
+	static constexpr uint64_t RYO_LONG_ADDRESS_BASE58_PREFIX = 0x8a20fd299;			// ShaDowS
+	static constexpr uint64_t RYO_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0xf11afd299; // ShaDoeS
+	static constexpr uint64_t RYO_LONG_SUBADDRESS_BASE58_PREFIX = 0x120fd299;			// ShaDos
 
-	static constexpr uint16_t P2P_DEFAULT_PORT = 12210;
-	static constexpr uint16_t RPC_DEFAULT_PORT = 12211;
-	static constexpr uint16_t ZMQ_RPC_DEFAULT_PORT = 12212;
+	static constexpr uint16_t P2P_DEFAULT_PORT = 19743;
+	static constexpr uint16_t RPC_DEFAULT_PORT = 19744;
+	static constexpr uint16_t ZMQ_RPC_DEFAULT_PORT = 19745;
 
 	//Random UUID generated from radioactive cs-137 ( http://www.fourmilab.ch/hotbits/how3.html ) it gives me a nice warm feeling =) 
-	static constexpr boost::uuids::uuid NETWORK_ID = { { 0xcd, 0xac, 0x50, 0x2e, 0xb3, 0x74, 0x8f, 0xf2, 0x0f, 0xb7, 0x72, 0x18, 0x0f, 0x73, 0x24, 0x13 } }; 
+	static constexpr boost::uuids::uuid NETWORK_ID = { { 0x04, 0x06, 0xdf, 0xce, 0xfc, 0x7c, 0x27, 0x4a, 0x24, 0xd4, 0xf3, 0x8d, 0x41, 0x42, 0x43, 0x41 } }; 
 
 	static constexpr const char *GENESIS_TX =
-		"023c01ff0001808098d0daf1d00f028be379aa57a70fa19c0ee5765fdc3d2aae0b1034158f4963e157d9042c24fbec21013402fc7071230f1f86f33099119105a7b1f64a898526060ab871e685059c223100";
+		"013c01ff00018094ebdc0302bc7ae26c871f8f18415e0fbd4776f9d4dbd5297c126bd99e26cf218ea0d6ec4a2101450a7665ca17a5ef3c691898068994569bb033012598c43068161b1194592327";
 	static constexpr uint32_t GENESIS_NONCE = 10000;
 
 	////////////////////// Dev fund constants
@@ -302,24 +302,24 @@ struct config<MAINNET>
 template <>
 struct config<TESTNET>
 {
-	static constexpr uint64_t LEGACY_LONG_ADDRESS_BASE58_PREFIX = 0x37751a;			   // Suto
-	static constexpr uint64_t LEGACY_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x34f51a; // Suti
-	static constexpr uint64_t LEGACY_LONG_SUBADDRESS_BASE58_PREFIX = 0x1d351a;		   // Susu
+	static constexpr uint64_t LEGACY_LONG_ADDRESS_BASE58_PREFIX = 0x211f;			   // Te
+	static constexpr uint64_t LEGACY_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x629f; // Ti
+	static constexpr uint64_t LEGACY_LONG_SUBADDRESS_BASE58_PREFIX = 0x59a0;		   // Ts
 
-	static constexpr uint64_t RYO_KURZ_SUBADDRESS_BASE58_PREFIX = 0x2ae192;			// RYoG
-	static constexpr uint64_t RYO_KURZ_ADDRESS_BASE58_PREFIX = 0x2b6192;			// RYoH
-	static constexpr uint64_t RYO_LONG_ADDRESS_BASE58_PREFIX = 0x306192;			// RYoT
-	static constexpr uint64_t RYO_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x29e192; // RYoE
-	static constexpr uint64_t RYO_LONG_SUBADDRESS_BASE58_PREFIX = 0x30e192;			// RYoU
+	static constexpr uint64_t RYO_KURZ_SUBADDRESS_BASE58_PREFIX = 0x3fa0;			  // Tr
+	static constexpr uint64_t RYO_KURZ_ADDRESS_BASE58_PREFIX = 0x7420;			  // Tu
+	static constexpr uint64_t RYO_LONG_ADDRESS_BASE58_PREFIX = 0x7d1f;			  // Tk
+	static constexpr uint64_t RYO_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x9f; // Tb
+	static constexpr uint64_t RYO_LONG_SUBADDRESS_BASE58_PREFIX = 0x1a1;		  // Tw
 
-	static constexpr uint16_t P2P_DEFAULT_PORT = 13310; 
-	static constexpr uint16_t RPC_DEFAULT_PORT = 13311; 
-	static constexpr uint16_t ZMQ_RPC_DEFAULT_PORT = 13312; 
+	static constexpr uint16_t P2P_DEFAULT_PORT = 29743; 
+	static constexpr uint16_t RPC_DEFAULT_PORT = 29744; 
+	static constexpr uint16_t ZMQ_RPC_DEFAULT_PORT = 29745; 
  
-	static constexpr boost::uuids::uuid NETWORK_ID = { { 0x6f, 0x81, 0x7d, 0x7e, 0xa2, 0x0b, 0x71, 0x77, 0x22, 0xc8, 0xd2, 0xff, 0x02, 0x5d, 0xe9, 0x92 } }; 
+	static constexpr boost::uuids::uuid NETWORK_ID = { { 0x12, 0x04, 0x06, 0xdf, 0xce, 0xfc, 0x7c, 0x27, 0x4a, 0x24, 0xd4, 0xf3, 0x41, 0x44, 0x44, 0x43 } }; 
 
 	static constexpr const char *GENESIS_TX =
-		"023c01ff0001808098d0daf1d00f028be379aa57a70fa19c0ee5765fdc3d2aae0b1034158f4963e157d9042c24fbec21013402fc7071230f1f86f33099119105a7b1f64a898526060ab871e685059c223100";
+		"013c01ff0001bbbad6adf00d029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101b20e782a04028cf9326ccd66e0d683f50dd1b261f4e81fac4bfa0c73d0601102";
 	static constexpr uint32_t GENESIS_NONCE = 10001;
 	
 	////////////////////// Dev fund constants
@@ -333,24 +333,24 @@ struct config<TESTNET>
 template <>
 struct config<STAGENET>
 {
-	static constexpr uint64_t LEGACY_LONG_ADDRESS_BASE58_PREFIX = 0x37751a;			   // Suto
-	static constexpr uint64_t LEGACY_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x34f51a; // Suti
-	static constexpr uint64_t LEGACY_LONG_SUBADDRESS_BASE58_PREFIX = 0x1d351a;		   // Susu
+	static constexpr uint64_t LEGACY_LONG_ADDRESS_BASE58_PREFIX = 0x2419;			   // Se
+	static constexpr uint64_t LEGACY_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x6599; // Si
+	static constexpr uint64_t LEGACY_LONG_SUBADDRESS_BASE58_PREFIX = 0x5c1a;		   // Ss
 
-	static constexpr uint64_t RYO_KURZ_SUBADDRESS_BASE58_PREFIX = 0xdc2192;			  // RYosG
-	static constexpr uint64_t RYO_KURZ_ADDRESS_BASE58_PREFIX = 0x1fc2192;			  // RYosK
-	static constexpr uint64_t RYO_LONG_ADDRESS_BASE58_PREFIX = 0xd1c2192;			  // RYosT
-	static constexpr uint64_t RYO_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x1fbbe192; // RYosE
-	static constexpr uint64_t RYO_LONG_SUBADDRESS_BASE58_PREFIX = 0xe3c2192;		  // RYosU
+	static constexpr uint64_t RYO_KURZ_SUBADDRESS_BASE58_PREFIX = 0x4f1a;			  // Sr
+	static constexpr uint64_t RYO_KURZ_ADDRESS_BASE58_PREFIX = 0x769a;			  // Su
+	static constexpr uint64_t RYO_LONG_ADDRESS_BASE58_PREFIX = 0x9a;			  // Sk
+	static constexpr uint64_t RYO_LONG_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x99; // Sb
+	static constexpr uint64_t RYO_LONG_SUBADDRESS_BASE58_PREFIX = 0x49b;		  // Sw
 
-	static constexpr uint16_t P2P_DEFAULT_PORT = 14410; 
-	static constexpr uint16_t RPC_DEFAULT_PORT = 14411; 
-	static constexpr uint16_t ZMQ_RPC_DEFAULT_PORT = 14412; 
+	static constexpr uint16_t P2P_DEFAULT_PORT = 39743; 
+	static constexpr uint16_t RPC_DEFAULT_PORT = 39744; 
+	static constexpr uint16_t ZMQ_RPC_DEFAULT_PORT = 39745; 
  
-	static constexpr boost::uuids::uuid NETWORK_ID = { { 0x15, 0x77, 0x3a, 0x26, 0x42, 0xa0, 0x3f, 0xf3, 0xe5, 0x79, 0x72, 0x8d, 0x4e, 0x5a, 0xf2, 0x98 } }; 
+	static constexpr boost::uuids::uuid NETWORK_ID = { { 0x12, 0x04, 0x06, 0xdf, 0xae, 0xfc, 0x7a, 0x27, 0x4c, 0x24, 0xd4, 0xf3, 0x41, 0x44, 0x44, 0x43 } }; 
 
 	static constexpr const char *GENESIS_TX =
-		"013c01ff0001ffffffffffff0302df5d56da0c7d643ddd1ce61901c7bdc5fb1738bfe39fbe69c28a3a7032729c0f2101168d0c4ca86fb55a4cf6a36d31431be1c53a3bd7411bb24e8832410289fa6f3b";
+		"";
 	static constexpr uint32_t GENESIS_NONCE = 10002;
 	
 	////////////////////// Dev fund constants
